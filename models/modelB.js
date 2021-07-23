8026d5ecd5d63118807ab3007a49a0d6eab9ee85
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-07-23 14:50:01 -0400

    Complete modelB challenge

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  brand: 'Audi',
+  model: 'TTS',
+  year: 2016,
+  mileage: 58967,
+  isRunning: true,
+  isForSale: false,
+  colorOptions: ['Daytona Grey','Ara Blue','Ibis White','Pulse Orange','Mythos Black', 'Tango Red'],
+  rimOptions: ['20in Audi Sport 10-Y Spoke','Audi Black Optic','18in 10-V Spoke']
+}
