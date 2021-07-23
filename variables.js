55a3f3eb3110c80c492bf68917517c5d1a4308de
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-07-23 14:06:32 -0400

    Complete variables.js assignment

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = 'Joel Embiid';
+var isActive = true;
+famousPerson = 'Ben Simmons';
+var num3 = 3;
+var num9 = 9;
+var sum = num3 + num9;
+var animals = ['Cat','Dog','Horse'];
+animals.shift();
+famousPerson.length;
