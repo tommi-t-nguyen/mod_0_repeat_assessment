8c526e4950a57eac90a197521cfed71e76ae918f
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-07-23 14:35:48 -0400

    Complete modelA challenge

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  hub: 'PHL',
+  location: 'Philadelphia',
+  numberOfTerminal: 5,
+  numberOfAirlines: 7,
+  isOpen: true,
+  isClosed: false,
+  airlineNames: ['Delta','American Airline','Southwest','Frontier','Spirit','United','JetBlue'],
+  foodVendors: ['Dunkin Donut','KFC','Mcdonalds']
+}
