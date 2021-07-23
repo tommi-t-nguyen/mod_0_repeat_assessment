68bcd065a629016671e2608d8dd308f4921280d6
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-07-23 14:10:14 -0400

    Complete program specific challenge

--- variables.js
+++ variables.js
@@ -1,6 +1,8 @@
 var famousPerson = 'Joel Embiid';
+console.log(famousPerson);
 var isActive = true;
 famousPerson = 'Ben Simmons';
+console.log(famousPerson);
 var num3 = 3;
 var num9 = 9;
 var sum = num3 + num9;
